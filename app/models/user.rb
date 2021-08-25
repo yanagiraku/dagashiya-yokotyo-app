cbeaf8c3a8e518c8599b4c969e5f9986d657931c
yanagiraku <n.miu.ovo.november19@gmail.com>
2021-08-25 15:58:34 +0900

    devise導入

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :password,        length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :nickname,        presence: true 
+  validates :age_id,          numericality: { other_than: 1 }
+
+
+  has_many :products
+  has_many :memories
+end
