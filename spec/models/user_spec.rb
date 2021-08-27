fd16749d8abf224b1b5013696f426b67867d6d39
yanagiraku <n.miu.ovo.november19@gmail.com>
2021-08-27 15:58:59 +0900

    単体テストコード

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,78 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it "nicknameとemailとpasswordとpassword_confirmationとage_idが存在すれば登録できる" do
+        expect(@user).to be_valid
+      end
+      it "passwordが6文字以上だと登録できる" do
+        @user.password = "aaa111"
+        @user.password_confirmation = "aaa111"
+        expect(@user).to be_valid
+      end
+    end
+
+    context '新規登録できない場合' do
+      it "nicknameが空では登録できない" do
+        @user.nickname = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "emailが空では登録できない" do
+        @user.email = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it "passwordが空では登録できない" do
+        @user.password = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it "age_idが未選択では登録できない" do
+        @user.age_id = 1
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Age must be other than 1")
+      end
+      it "emailに@が含まれていないと登録できない" do
+        @user.email = "aaaaaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+      it "passwordとpassword_confirmationが一致しない場合は登録できない" do
+        @user.password = "aaa111"
+        @user.password_confirmation = "aaa222"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it "passwordが5文字以下であれば登録できない" do
+        @user.password = "aa111"
+        @user.password_confirmation = "aa111"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+      it "passwordは、半角英数字混合での入力が必須である(全角のみ入力)" do
+        @user.password = "パスワードです"
+        @user.password_confirmation = "パスワードです"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      it "passwordは、半角英数字混合での入力が必須である(英字のみ入力)" do
+        @user.password = "aaaaaa"
+        @user.password_confirmation = "aaaaaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      it "passwordは、半角英数字混合での入力が必須である(数字のみ入力)" do
+        @user.password = "111111"
+        @user.password_confirmation = "111111"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+    end
+  end
+end
